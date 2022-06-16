8365f459018e5af1220b60884bce4d0720b103bd
mpoapostolis <mpoapostolis@gmail.com>
2022-06-16 13:49:57 +0300

    initiate with tailwindcss

---
+++ apps/docs/postcss.config.js
@@ -0,0 +1,8 @@
+const postCss = require('../../packages/config/postcss.config');
+
+/*
+apps/web/tailwind.config.js
+apps/docs/tailwind.config.js
+packages/ui/tailwind.config.js
+*/
+module.exports = postCss
