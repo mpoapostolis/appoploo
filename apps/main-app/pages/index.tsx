12346673ed746ee6b1089fb54a4cf38c68246bfb
mpoapostolis <mpoapostolis@gmail.com>
2022-06-18 17:44:02 +0300

    initialize project

---
+++ apps/main-app/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Login } from "ui/login";
+
+export default function Docs() {
+  return (
+    <Login bg="https://images.unsplash.com/photo-1542397284385-6010376c5337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" />
+  );
+}
---
+++ apps/main-app/tailwind.config.js
@@ -0,0 +1,8 @@
+const tailwindConfig = require('../../packages/config/tailwind.config');
+
+/*
+apps/web/tailwind.config.js
+apps/docs/tailwind.config.js
+packages/ui/tailwind.config.js
+*/
+module.exports = tailwindConfig
